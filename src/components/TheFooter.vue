c5e4798a0ed9dd85327b774bc436dcc3fcb12e6c
Tofu-Xx <1714843907@qq.com>
2024-09-16 23:51:55 +0800

    chore: github link

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <nav flex="~ gap2">
-    <a href="https://github.com/Tofu-Xx/tempt/blob/main/webpage/README.md" i-carbon:logo-github></a>
+    <a href="https://github.com/Tofu-Xx/deskdown" i-carbon:logo-github></a>
     <button @click="toggleDark()"  i-carbon:sun dark:i-carbon:moon></button>
   </nav>
 </template>
