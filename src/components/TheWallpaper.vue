21e621809d497d9ee0d417c0f9c88cd29dacce3d
Tofu-Xx <1714843907@qq.com>
2024-09-25 01:25:15 +0800

    style: update

--- src/components/TheWallpaper.vue
+++ src/components/TheWallpaper.vue
@@ -3,5 +3,5 @@ const src = unref(wallpaperSrc)||"https://picsum.photos/3840/2160"
 readyNext('ok')
 </script>
 <template>
-  <img :src pos="absolute inset-0 -z-100" size-screen :class="{'hidden':!isWallpaper}">
+  <img :src pos="absolute inset-0 -z-100" size-screen object-cover :class="{'hidden':!isWallpaper}">
 </template>
