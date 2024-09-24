005d37f336d07f214c7fa614ea7db570f80aae63
Tofu-Xx <1714843907@qq.com>
2024-09-25 01:26:27 +0800

    chore: log

--- src/composables/wallpaper.ts
+++ src/composables/wallpaper.ts
@@ -2,6 +2,7 @@ export let wallpaperSrc = $(useStorage("wallpaper-base64", ""));
 export const isWallpaper = useStorage("use-wallpaper", false);
 export const toggleWallpaper = useToggle(isWallpaper);
 export const readyNext = async (msg: string) => {
+  console.log('ready next img... awaiting...')
   const { data } = await useFetch("https://picsum.photos/3840/2160").blob();
   const { base64 } = $(useBase64(data as any));
   whenever($$(base64), () => {
