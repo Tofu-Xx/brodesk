97df436ab1ccc5fc5ea9bfec88193d32ae496d97
Tofu-Xx <1714843907@qq.com>
2024-09-25 02:23:57 +0800

    perf: select tag

--- src/components/TheInput.vue
+++ src/components/TheInput.vue
@@ -3,10 +3,8 @@ import { Locator } from "~/data/locators";
 export const locators:Locator[] = []
 const lctrKey = 'current-locator';
 let lctrIdx = $(useStorage(lctrKey,0))
-let locator = $computed({
-  get:()=>locators[lctrIdx % locators.length],
-  set:v=>lctrIdx=locators.indexOf(v)
-})
+let locator = $computed(()=>locators[lctrIdx])
+// set:v=>lctrIdx=locators.indexOf(v)
 
 let title = $(useTitle())
 watchEffect(()=>{
@@ -18,23 +16,7 @@ const iptRef = $(useTemplateRef('ipt'))
 onStartTyping(()=>{
   iptRef?.focus()
 })
-// onMounted(() => {
-//   iptRef?.focus()
-// })
-// const {ctrl_k} = useMagicKeys()
-// watch(ctrl_k,(v)=>{
-//   if(v){
-//     v.preventDefault()
-//     iptRef?.focus()
-//   }
-// })
 
-// useEventListener(document,'keydown', function(event) {
-//   if (event.ctrlKey && event.key === 'k') {
-//     event.preventDefault();
-//     iptRef?.focus()
-//   }
-// });
 const go = () => {
   search(locator.rawurl,q)
   q = ''
@@ -54,22 +36,23 @@ const doTab = () =>{
 
 <template>
   <div pos-absolute top-25 w-full flex="~ justify-center gap2">
-    <select ::="locator" bg-hex-8883 rounded-4 pl4>
-      <option v-for="(el) of locators" :value="el">{{el.name}}</option>
+    <select ::="lctrIdx" min-w12 text-center bg-hex-8883 rounded-2 outline-none appearance-none>
+      <option v-for="(el,i) of locators" :value="i" text-black>{{el.name}}</option>
     </select>
     <form @submit.prevent="go"
       h10 flex min-w300px w="33%">
       <input
         type="text"
         ref="ipt"
+        tabindex="1"
         ::="q"
         :placeholder="locator.rawurl"
         @keydown.tab.prevent="doTab"
         holder="op-50 black dark:op-50 dark:white"
         px4 bg-hex-8883 flex-1
-        rounded-l-4
+        rounded-l-2
         outline="none active:none">
-      <button px4 rounded-r-4 shadow="inner hex-8883 active:hex-8881" outline-none transition>Search</button>
+      <button px4 rounded-r-2 shadow="inner hex-8883 active:hex-8881" outline-none transition>Search</button>
     </form>
   </div>
 </template>
