98c7cb5d8031b1dde8fa4025134684ded364b681
Tofu-Xx <1714843907@qq.com>
2024-09-25 03:29:16 +0800

    chore: add ico

--- components.d.ts
+++ components.d.ts
@@ -7,9 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    ICarbonLogoGithub: typeof import('~icons/carbon/logo-github')['default']
-    ICarbonSun: typeof import('~icons/carbon/sun')['default']
-    TheCounter: typeof import('./src/components/TheCounter.vue')['default']
     TheFooter: typeof import('./src/components/TheFooter.vue')['default']
     TheInput: typeof import('./src/components/TheInput.vue')['default']
     TheWallpaper: typeof import('./src/components/TheWallpaper.vue')['default']
