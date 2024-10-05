376e1e577b3da577b1d6eb7fbefc67aa220a244f
Tofu-Xx <1714843907@qq.com>
2024-10-05 14:52:56 +0800

    style: base64

--- src/components/TheWallpaper.vue
+++ src/components/TheWallpaper.vue
@@ -1,20 +1,7 @@
 <script setup lang="ts">
 const src = ref<string | undefined>(get(currentWallpaper_) || get(base64Wallpapers_).pop())
-if (!get(src)) {
-  set(src, imgUrl)
-  // const imgRef = useTemplateRef('img')
-  // onMounted(() => {
-  //   // const { base64 } = useBase64(imgRef)
-  //   // console.log('base64', base64)
-  //   // watch(base64, () => {
-  //   //   set(src, get(base64))
-  //   //   console.log('ok src', get(src))
-  //   // })
-  //   console.dir(imgRef)
-  // })
-  provide('base64', void 0)
-}
-else provide('base64', src)
+provide('base64', unref(src))
+get(src) || set(src, imgUrl)
 </script>
 
 <template>
